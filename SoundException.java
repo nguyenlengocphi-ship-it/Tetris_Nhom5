24d5f0efe1d10cfde211b10eda4b5eb41ab6335f
thuytrinh16 <thuytrinht16207@gmail.com>
2026-04-01 21:05:49 +0700

    Trinh: cập nhật file

---
+++ SoundException.java
@@ -0,0 +1,8 @@
+package utils;
+
+// Custom Exception kế thừa từ Exception
+public class SoundException extends Exception {
+    public SoundException(String message) {
+        super(message);
+    }
+}
