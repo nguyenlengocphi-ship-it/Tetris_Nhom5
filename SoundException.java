3253178b3846a0a3862516686f053058f8496118
nguyenlengocphi-ship-it <nguyenlengocphi@gmail.com>
2026-04-01 21:14:01 +0700

    Delete SoundException.java

--- SoundException.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-// Custom Exception kế thừa từ Exception
-public class SoundException extends Exception {
-    public SoundException(String message) {
-        super(message);
-    }
-}
